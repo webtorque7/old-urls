d7f1fed655d0f6bcce2953d13aa000bcd8266fba
Conrad <conrad@webtorque.co.nz>
2014-02-11 12:04:35 +1300

    Add model admin

---
+++ code/OldURLAdmin.php
@@ -0,0 +1,9 @@
+<?php
+
+class OldURLAdmin extends ModelAdmin {
+	private static $managed_models = array('OldURL');
+
+	private static $menu_title = 'Old URLs';
+
+	private static $url_segment = 'old-urls';
+} 
